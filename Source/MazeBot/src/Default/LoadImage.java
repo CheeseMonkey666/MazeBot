190d002f5ffe1b5f68c07b5ce8a333509775a390
CheeseMonkey666 <cheesemonkeycode@gmail.com>
2015-07-16 19:55:38 +0100

    More Speed, More Options

--- Source/MazeBot/src/Default/LoadImage.java
+++ Source/MazeBot/src/Default/LoadImage.java
@@ -15,37 +15,39 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class LoadImage implements ActionListener {
-	
-	
-	public LoadImage(){
-		
+
+	public LoadImage() {
+
 	}
-	
-	public void actionPerformed(ActionEvent e){
+
+	public void actionPerformed(ActionEvent e) {
 		FileDialog fd;
 		String fileName;
 		BufferedImage img;
 		fd = new FileDialog(Main.parentFrame, "Open");
 		fd.setMultipleMode(false);
-		fd.setFile("*.bmp");
+		fd.setFile("*.bmp;*.png");
 		fd.setDirectory("C:\\");
 		fd.setVisible(true);
 		fileName = fd.getDirectory() + fd.getFile();
 		try {
 			img = ImageIO.read(new File(fileName));
-			if (img.getHeight() <= 900 && img.getWidth() <= 600){
+			if (img.getHeight() <= 900 && img.getWidth() <= 600) {
 				Main.map = img;
-				Image image = (Image)img;
-				int h = img.getHeight(), w = img.getWidth(), scale = Math.min(600 / h, 900 / w);
-				image = img.getScaledInstance(w * scale, h * scale, Image.SCALE_DEFAULT);
+				Image image = (Image) img;
+				int h = img.getHeight(), w = img.getWidth(), scale = Math.min(
+						600 / h, 900 / w);
+				image = img.getScaledInstance(w * scale, h * scale,
+						Image.SCALE_DEFAULT);
 				Main.Canvas.setIcon(new ImageIcon(image));
-				Main.Canvas.setBorder(BorderFactory.createLineBorder(Color.black));
-			}
-			else{
-				JOptionPane.showMessageDialog(Main.parentFrame, "Image too large (900x600 max)");
+				Main.Canvas.setBorder(BorderFactory
+						.createLineBorder(Color.black));
+			} else {
+				JOptionPane.showMessageDialog(Main.parentFrame,
+						"Image too large (900x600 max)");
 				img = null;
 			}
-			} catch (IOException e1) {
+		} catch (IOException e1) {
 			JOptionPane.showMessageDialog(Main.parentFrame, "Image not loaded");
 			img = null;
 		}
